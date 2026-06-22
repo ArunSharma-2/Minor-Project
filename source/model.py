7de53b88a8d7c59f1208d48c67c8abaacdb9fd19
ArunSharma-2 <sharun4745@gmail.com>
2026-06-22 13:54:00 +0530

    printing accuracy

--- source/model.py
+++ source/model.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import sklearn.linear_model as lm
+from sklearn.model_selection import train_test_split
 
 
 
@@ -9,21 +10,17 @@ import sklearn.linear_model as lm
 data = pd.read_csv('dataSet/student-mat.csv')
 db = data[['G1', 'G2', 'G3', 'studytime', 'failures', 'absences']]
 
-x = db.drop('G3', axis=1)
-y = db['G3']
+x_train , x_test, y_train, y_test = train_test_split(db[['G1', 'G2', 'studytime', 'failures', 'absences']], db['G3'], test_size=0.3, random_state=42)
 
 # ---------------------------------MODEL TRAINING---------------------------------
 model = lm.LinearRegression()
-model.fit(x, y)
+model.fit(x_train, y_train)
 
 # ---------------------------------MODEL PREDICTION---------------------------------
-predictions = model.predict(x)
+predictions = model.predict(x_test)
 print(predictions)
 
 # ---------------------------------MODEL EVALUATION---------------------------------
-plt.scatter(db['G1'], db['G3'], color='blue')
-plt.scatter(db['G2'], db['G3'], color='red')
-plt.xlabel('G1')
-plt.ylabel('G3')
-plt.title('Linear Regression')
-plt.show()
+
+accuracy = model.score(x_test, y_test)
+print(f"Model Accuracy: {accuracy * 100:.2f}%")
